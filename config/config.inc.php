7837a03d75adca127533b4a5d50a906ae9fe0368
brad2014 <brad2014@users.noreply.github.com>
2016-12-04 16:01:43 -0800

    initial rev

---
+++ config/config.inc.php
@@ -0,0 +1,12 @@
+<?php
+
+#
+# See default_config.php for what can be set, and what the defaults are.
+#
+$default_config =  Array();
+
+# connection to database server
+$default_config['db_password'] = 'changeme!';   # Don't forget to change!
+
+# default widget colors
+$default_config['colors'] = Array( 'red', 'green' );
